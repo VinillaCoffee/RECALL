1bef7d7c1e08f6f768021f00fda63a72cc1f82fd
Vini <204002790@qq.com>
2025-04-14 21:15:52 +0800

    add_exp_results

--- run_cl.sh
+++ run_cl.sh
@@ -39,11 +39,9 @@ CUDA_VISIBLE_DEVICES=0 python3 run_cl.py --tasks=CW6_0 --seed=0 --steps_per_task
   --use_multi_layer_head=True --use_popArt=False --agent_policy_exploration=True --carried_critic=True
 
 # RECALL
-$env:CUDA_VISIBLE_DEVICES="0"
 python run_cl.py --tasks=CW10 --seed=0 --steps_per_task=5e5 --log_every=1e4 --cl_method=recall --batch_size=128 --replay_size=5e6 --behavior_cloning=True --policy_reg_coef=10.0 --use_multi_layer_head=True --use_popArt=True --agent_policy_exploration=True --carried_critic=True
                                                                                                                                           
 # test:ClonEx-SAC
-$env:CUDA_VISIBLE_DEVICES="0"
 python run_cl.py --tasks=CW10 --seed=0 --steps_per_task=5e5 --log_every=1e4 --cl_method=clonex --policy_reg_coef=100.0 --agent_policy_exploration=True --clipnorm=0.1
 
 # test:EWC
